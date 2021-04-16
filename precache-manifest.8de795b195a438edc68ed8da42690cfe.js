5d2702c98aad58bdce230d3840646c38fafc4782
Rishav Jyoti Bordalai <49098410+rishavjyoti@users.noreply.github.com>
2021-04-16 09:48:02 +0530

    Updates

---
+++ precache-manifest.8de795b195a438edc68ed8da42690cfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c2251b85a3621923d46f90f2026a4b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "62bfd2d2787dabf02528",
+    "url": "/portfolio/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/portfolio/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bfd2d2787dabf02528",
+    "url": "/portfolio/static/js/main.b198652e.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "edc0af15cf8ce19754b3207325fc65e2",
+    "url": "/portfolio/static/media/resume.edc0af15.pdf"
+  },
+  {
+    "revision": "20e7372fbf0e1688612c5a0d4eeb7e54",
+    "url": "/portfolio/static/media/rishav.20e7372f.png"
+  }
+]);
