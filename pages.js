0e449200c3c55590989dcc8e66e77734a4aac8cc
Rishav Jyoti Bordalai <49098410+rishavjyoti@users.noreply.github.com>
2021-04-16 09:52:49 +0530

    readme changed

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/rishavjyoti/rishavjyoti.github.io.git";
+const repoURL = "https://github.com/rishavjyoti/portfolio.git";
 
 ghpages.publish(
   pathname,
